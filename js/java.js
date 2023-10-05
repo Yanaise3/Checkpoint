caf9cafd32bf0817d340a92388e772f3bdfbd440
yanaise <yayadiomande333@gmail.com>
2023-10-05 10:53:04 +0000

    ajout de style css

---
+++ js/java.js
@@ -0,0 +1,28 @@
+// {
+// let x=5;
+// let y=20;
+// let resultat=x*y;
+// console.log("le resultat est: ",resultat);
+// }
+
+
+// let x=100;
+// let y=50;
+// let z=40;
+// if (x>y){
+//     let resultat=y+z
+//     console.log(" voici le resultat",resultat);}
+// else if (z > x || y){
+//     let resultat=x+y;
+//     console.log(resultat);}
+// else{console.log("tout est faux");}
+
+
+// function pair(n){
+//     if (n% 2==0){return true;}
+//     else{return false;}
+// }
+// let nombre=pair(19);
+// if (nombre){console.log("le nombre est pair");}
+// else {console.log("le nombre est impair");}
+
